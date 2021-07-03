4b3868de657f5c0c92e12b528f1e0c538b1266df
Minecraft806 <82305682+Minecraft806@users.noreply.github.com>
2021-07-03 13:04:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+img = "";
+status = "";
+
 function setup() {
     canvas=createCanvas(640,420);
     canvas.center()
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("CocoSsd Model Has Been Loaded");
+    status = true;
+    object_detector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+    }
 }
 
 function draw() {
@@ -10,6 +29,12 @@ function draw() {
       noFill();
       stroke("blue")
       rect(100,200,200,200);
+
+      fill("red");
+      text("Birds", 400, 200);
+      noFill();
+      stroke("red")
+      rect(350, 180, 150, 150);
 }
 
 function preload() {
